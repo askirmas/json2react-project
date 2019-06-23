30275e56835b67c830fbb7775582f8c5a332b455
askirmas <kirmas@phystech.edu>
2019-06-23 23:51:21 +0300

    Move redirection to router as command

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,22 @@ import Layout from '../layout';
 import renderJson from '../components/utils/renderJson'
 import Head from 'next/head'
 
-const homePage = Object.keys(require('../pages.json'))[0]
-
-function Service({content, redirect}) {
+function Service({page, redirect}) {
   if (redirect)
     return <Head>
-      <meta http-equiv="Refresh" content={`0; url=./${homePage}/`}/>
+      <meta http-equiv="Refresh" content={`0; url=./${page}/`}/>
     </Head>
-  return <Layout>{content}</Layout>
+  return <Layout>{page}</Layout>
 }
 
 Service.getInitialProps = function({query: {json, redirect}}) {
   if (redirect)
-    return {redirect}
+    return {redirect, page: json}
   let content = require(`./${json}.json`)
   try {
     content = require(`./${json}`).default(content)
   } catch(e) {}
-  return { content: renderJson(content) }
+  return { page: renderJson(content) }
 }
 
 export default withRouter(Service)
